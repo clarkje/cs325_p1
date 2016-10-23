bc3a01c300648c66a7fd92135ba380ab80d1dbd3
clarkje <clarkje@oregonstate.edu>
2016-10-23 16:34:40 -0700

    As I was trying to come up with the recurrence, I realized that I didn't fully understand the code, and that the naming was confusing.  I think this is much more straightfoward.

--- dynMinCoins.py
+++ dynMinCoins.py
@@ -3,51 +3,65 @@
 # Project 2
 # Adapted from HW4 group code
 
-# minCoins
+# changedp
 # Implements a dynamic programming algorithm that returns the minimum number
 # of coins required to make an arbitrary amount of change
 
-def minCoins(coinArray, amount):
+import math
 
- # Base Case
- if (amount == 0):
-     return 0
+def changedp(coinArray, amount):
 
- # The worst-case scenario is that we return all pennies
- # Build an array big enough to hold pennies for the whole amount
- candidateArr = [0] * (amount + 1)
+    # Base Case
+    if (amount == 0):
+        return 0
 
- # build the amount out with pennies
- # then consolidate pennies to nickels to dimes to quarters until we can't
- # consolidate anymore
- for eachCent in range(amount+1):
+    # Create an array, which stores the minimum number of coins for
+    # each possible value from 1 ... amount
 
-    currentMinCoins = eachCent
+    # Technically, the first element should be 0, but since we evaluate the base case
+    # first, we'll never consider it anyway...
+    minCoinArray = [float("inf")] * (amount + 1)
 
-    # Evaluate each of the available coins that's smaller or equal to the amount
-    for currentCoin in [coin for coin in coinArray if coin <= eachCent]:
+    # Compute the minimum coins required for all values from 1 to amount
+    for value in range(amount + 1):
 
-        if candidateArr[eachCent - currentCoin] + 1 < currentMinCoins:
-            currentMinCoins = 1 + candidateArr[eachCent - currentCoin]
+        # Populate special cases to get things started
+        if (value == 0):
+            minCoinArray[0] = 0;
+        elif (value == 1):
+            minCoinArray[1] = 1;
+        else:
+            # Consider all coins smaller than value
+            for coin in coinArray:
+                # If the coin is less than or equal to the value of the element we're considering
+                if (coin <= value):
+                    # Check the already-computed minCoin value at this element,
+                    # less the value of the coin we're considering
+                    minCoins = minCoinArray[value - coin] + 1;
+                    # If that result is not our sentinel value (float(inf)) and
+                    # the number of coins is less than the number of smaller coins
+                    # used to compute the value, then we'll take it
+                    if (minCoins != float("inf") and minCoins < minCoinArray[value]):
+                        minCoinArray[value] = minCoins
 
-        candidateArr[eachCent] = currentMinCoins
-
- return candidateArr[amount]
+    # Once we've computed the minimum coins required, we can just get the
+    # value from the last element
+    return minCoinArray[amount]
 
 # Main
 
 def main():
  coinArray = [1, 5, 10, 25]
- # changeToCheck = [0, 1, 5, 7, 12, 15, 23, 44, 68, 75, 99, 3, 10, 15, 16]
- changeToCheck = [10]
+ changeToCheck = [0, 1, 5, 7, 12, 15, 23, 44, 68, 75, 99, 3, 10, 15, 16]
+ # changeToCheck = [12]
 
 
  # Initialize an array to store our candidate values
  #candidateArr = [0] * (max(changeToCheck)+1)
 
  for change in changeToCheck:
-#     print("Change needed: %s Min Coins: %s" % (change, minCoins(candidateArr, coinArray, change)))
-     print("Change needed: %s Min Coins: %s" % (change, minCoins(coinArray, change)))
+#     print("Change needed: %s Min Coins: %s" % (change, changedp(ccoinArray, change)))
+     print("Change needed: %s Min Coins: %s" % (change, changedp(coinArray, change)))
 
 
 if __name__ == "__main__":
