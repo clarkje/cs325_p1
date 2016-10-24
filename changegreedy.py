8185121efeee3829c4522433a42cacb4a43fa156
Caleigh Runge-Hottman <crungehottman@users.noreply.github.com>
2016-10-24 16:17:48 -0600

    added file i/o

--- changegreedy.py
+++ changegreedy.py
@@ -1,3 +1,12 @@
+# Author: Caleigh Runge-Hottman 
+#    (with main from Jeromie Clark and Andrew Kittrell)
+# CS 325 - 400 Fall 2016
+# Project 2
+
+from __future__ import print_function
+import math
+import sys
+
 def changegreedy(V, A):
     denoms = V[::-1]  # reversed list of denominations
     changeToMake = A  # remaining change to make
@@ -14,16 +23,37 @@ def changegreedy(V, A):
     C.reverse() 
     return (C, m)
 
-def main():
-#    A = 15
-#    V = [1,2,4,8]
-#    A = 29
-    A = 31
-    V = [1, 3, 7, 12]
-    result = changegreedy(V, A)
 
-    print result[0]
-    print 'min number of coins: ', result[1]    
+# Main
+# Code by Jeromie Clark
+# Slightly edited by Andrew Kittrell and Caleigh Runge-Hottman
+
+def main():
+    filename = sys.argv[1]
+    inFile = filename + ".txt"
+    outFile = filename + "change.txt"
+
+    outFile = open(outFile,'w')
+    print("Algorithm: Greedy", file=outFile)
+
+    with open(inFile, 'r') as inFile:
+
+        for line in inFile:
+            if (line.count("[") > 0):
+
+                line = line.strip()
+                line = line.strip('[]')
+                ints = line.split(',')
+                coinArray = [int(i) for i in ints]
+
+            else:
+                change = int(line)
+                minCoinArray = changegreedy(coinArray, change)
+                print(minCoinArray, file=outFile)
+                totalCoins = 0
+                for i in minCoinArray:
+                    totalCoins += i
+                print(totalCoins, file=outFile) 
 
 if __name__ == "__main__":
     main()
